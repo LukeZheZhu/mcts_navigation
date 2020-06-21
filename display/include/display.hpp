b8c18b230c3744b3a618afce49c5f22b2ce328fb
LukeZhu <luke.zhezhu@gmail.com>
2020-06-22 07:13:16 +0800

    add display

---
+++ display/include/display.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "map.hpp"
+
+namespace nsDisplay {
+    struct sPoint {
+        int x;
+        int y;
+
+        sPoint() :
+            x(0),
+            y(0) {
+        };
+
+        sPoint(int pointX, int pointY) :
+            x(pointX),
+            y(pointY) {
+        };
+    };
+
+    class cDisplay {
+    public:
+        struct sPoint
+    } // class cDisplay
+} //namesapce nsDisplay
